811fec8c5a9c37fca9b07f9a917918b9be3ce3c6
Patrik František Balaz <patrik.f.balaz@outlook.com>
2019-07-19 19:59:21 +0200

    init

---
+++ src/components/app/app.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const app = <h1>Hello World!</h1>
+
+export default app;
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin }  = require('clean-webpack-plugin');
+const WriteFilePlugin  = require('write-file-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'build')
+    },
+    devtool: "inline-source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader'
+                    }
+                ]
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test:/\.(s*)css$/,
+                use:['style-loader','css-loader', 'sass-loader']
+              }
+        ]
+    },
+    plugins: [
+        new WriteFilePlugin(),
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebPackPlugin({
+            template: './public/index.html'
+        })
+    ]
+};
